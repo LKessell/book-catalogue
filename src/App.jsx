4bb500c84f6646ca4b2cf8bd58b50d8fa8b454e4
Lauren Kessell <lkessell1@gmail.com>
2023-08-13 14:37:18 -0400

    Import catalogue data, render BookList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 import './App.css';
 import Header from './components/Header/Header';
+import BookList from './components/BookList/BookList';
+
+import { catalogueData } from './data';
 
 function App() {
+  const [catalogue] = useState(catalogueData.products);
+
   return (
     <main>
       <Header />
+      <BookList data={catalogue} />
     </main>
   );
 }
