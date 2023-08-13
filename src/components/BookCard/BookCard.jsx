c70c2e7a51c1654c775b84a4be996eae98eb9848
Lauren Kessell <lkessell1@gmail.com>
2023-08-13 17:29:35 -0400

    Add book image, subtitle, and author

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+
+import './BookCard.css';
+import imagePlaceholder from '../../assets/icons8-image-64.png';
+
 function BookCard({ book }) {
-  return <li>{book.name}</li>;
+  const [imgSrc, setImgSrc] = useState(book.images ? book.images[0].uri : imagePlaceholder);
+
+  return (
+    <li className="card-wrapper">
+      <div>
+        <h2>
+          {book.name}
+          <br />
+          <span>{book.subName}</span>
+        </h2>
+        <h3>{book.author}</h3>
+      </div>
+      <img className="jacket-cover" src={imgSrc} onError={() => setImgSrc(imagePlaceholder)} />
+    </li>
+  );
 }
 
 export default BookCard;
